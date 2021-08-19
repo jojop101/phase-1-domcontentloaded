db2b7ec4e2aa126e077ab53f7bbc5a5b98c0a84d
GiuseppePennisi <jojop101@gmail.com>
2021-08-19 17:15:49 -0400

    commit tree 545e6d62d4e066f8ceeb1f6538fe55aba4448f62

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+// document.addEventListener('DOMContentLoaded', function () {
+//     updateDom()
+    
+// });
+
+// function updateDom(){
+//     document.querySelector('text')
+//     .textContent = 'This is really cool!'
+//}
+
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+ 
+ 
+  function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
